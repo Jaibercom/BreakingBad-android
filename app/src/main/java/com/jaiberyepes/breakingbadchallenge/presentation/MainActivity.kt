537111905695bc89a9e05ec9d128b7f21b61bd6b
Jaiber Yepes <Juan.X.Yepes.-ND@disney.com>
2020-08-07 14:45:26 -0500

    project setup

---
+++ app/src/main/java/com/jaiberyepes/breakingbadchallenge/presentation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.jaiberyepes.breakingbadchallenge.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jaiberyepes.breakingbadchallenge.R
+
+/**
+ * Activity for the Main Entry-Point.
+ *
+ * @author jaiber.yepes
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
